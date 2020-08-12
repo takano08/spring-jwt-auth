1e239364ebbc461c31387a9fb71c124730dcf174
TakamichiTatsumoto <t.t.tatsumoto@gmail.com>
2020-08-12 10:08:40 +0900

    user name を修正

--- src/main/java/com/nyasba/jwt/apiauth/service/UserDetailsServiceImpl.java
+++ src/main/java/com/nyasba/jwt/apiauth/service/UserDetailsServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.Set;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    private static List<String> usernameList = Arrays.asList("nyasba", "admin");
+    private static List<String> usernameList = Arrays.asList("test", "admin");
     private static String ENCRYPTED_PASSWORD = "$2a$10$5DF/j5hHnbeHyh85/0Bdzu1HV1KyJKZRt2GhpsfzQ8387A/9duSuq"; // "password"を暗号化した値
 
     @Override
