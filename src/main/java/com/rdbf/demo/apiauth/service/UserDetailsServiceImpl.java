ce2cdfc9789de20b7178d9ace6f1a2451996a6c1
TakamichiTatsumoto <t.t.tatsumoto@gmail.com>
2020-08-18 13:44:34 +0900

    logの追加 アノテーションを追加

--- src/main/java/com/rdbf/demo/apiauth/service/UserDetailsServiceImpl.java
+++ src/main/java/com/rdbf/demo/apiauth/service/UserDetailsServiceImpl.java
@@ -3,6 +3,7 @@ package com.rdbf.demo.apiauth.service;
 import com.rdbf.demo.apiauth.controller.SampleController;
 import com.rdbf.demo.apiauth.domain.People;
 import com.rdbf.demo.apiauth.repository.PeopleRepository;
+import org.apache.ibatis.annotations.AutomapConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,18 +13,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final PeopleRepository peopleRepository;
     private static final Logger LOGGER = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
 
-
+    @Autowired
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     public UserDetailsServiceImpl(PeopleRepository peopleRepository) {
         this.peopleRepository = peopleRepository;
     }
@@ -31,19 +28,14 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        // 本来ならここでDBなどからユーザを検索することになるが、サンプルのためリストに含まれるかで判定している
-//        if(!usernameList.contains(username)){
-//            throw new UsernameNotFoundException(username);
-//        }
-
         People people = peopleRepository.findByLoginId(username);
 
         if(people == null){
+            LOGGER.info("ユーザーが見つかりませんでした");
             throw new UsernameNotFoundException("User Not Found with -> username or email: " + username);
         }
 
-        LOGGER.info("ゆーざーねーむ"+username+":::::::::::people::::::"+people.getLoginId());
-
+        LOGGER.info("username:::::::::::"+username+":::::::::::Data::::::"+people.getLoginId());
 
         return User.withUsername(username)
                 .password(people.getPassword())
