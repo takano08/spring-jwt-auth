cc5a0fc8d87cdb036fbb8156bee218d65a688ff3
takano <ri1saxxminny83xx@gmail.com>
2020-08-22 11:46:04 +0900

    articlesのtableからselect,insert,update

--- src/main/java/com/rdbf/demo/apiauth/repository/ArticleRepository.java
+++ src/main/java/com/rdbf/demo/apiauth/repository/ArticleRepository.java
@@ -4,15 +4,22 @@ import com.rdbf.demo.apiauth.domain.Article;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
 import java.util.List;
 
 @Mapper
 public interface ArticleRepository {
     //articlesの指定したカラムをselectする
-    @Select("SELECT article_id,  FROM articles WHERE article_id = #{articleId}")
-    Article findByArticleId(Long articleId);
+    @Select("SELECT article_id,title,created_date,tag,body,owner,  FROM articles WHERE article_id = #{articleId}")
+    Article findByArticleId(Integer articleId);
 
-    @Select("SELECT article_id,  FROM articles")
+    @Select("SELECT article_id,title,created_date,tag,body,owner,  FROM articles")
     List<Article> findAll();
 
+    @Insert("INSERT INTO articles(article_id,title,created_date,tag,body,owner) VALUES (#{articleId},#{title},#{createdDate},#{tag},#{body},#{owner})")
+    void createArticle(Article createArticleData);
+
+    @Update("UPDATE articles SET title=#{title},created_date=#{createdDate},tag=#{tag},body=#{body},owner=#{owner}")
+    void updateArticle(Article updateArticleData);
 }
