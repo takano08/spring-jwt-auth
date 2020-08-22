6ae08015e401625eedfcf417431b6a3d3bf5314b
TakamichiTatsumoto <t.t.tatsumoto@gmail.com>
2020-08-23 06:35:27 +0900

    SQLのシンタックスの誤りの修正と、カラム名変更とインスタンスフィールド変更（desc →　inquiry）にともなうSQL文の修正

--- src/main/java/com/rdbf/demo/apiauth/repository/ApplicantRepository.java
+++ src/main/java/com/rdbf/demo/apiauth/repository/ApplicantRepository.java
@@ -8,8 +8,9 @@ import org.apache.ibatis.annotations.Mapper;
 public interface ApplicantRepository {
 
 
-    @Insert("INSERT INTO applicants(applicant_id,occupation,applicant_name,applicant_name_ruby,birth_day,gender,e_mail,phone_number,desc) VALUES (#{applicantId},#{occupation},#{applicantName},#{applicantNameRuby},#{birthDay},#{gender}),#{eMail},#{phoneNumber},#{desc}")
+    @Insert("INSERT INTO applicants(applicant_id,occupation,applicant_name,applicant_name_ruby,birth_day,gender,e_mail,phone_number,inquiry)" +
+                         "VALUES(#{applicantId},#{occupation},#{applicantName},#{applicantNameRuby},#{birthDay},#{gender},#{eMail},#{phoneNumber},#{inquiry})"
+    )
     void createApplicant(Applicant createApplicantData);
-
 
 }
