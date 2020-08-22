c07954ecc5c4b2bd4118e5cbbb3d4e3ccaa62d34
takano <ri1saxxminny83xx@gmail.com>
2020-08-22 11:44:24 +0900

    get,post,put定義

--- src/main/java/com/rdbf/demo/apiauth/controller/ArticleController.java
+++ src/main/java/com/rdbf/demo/apiauth/controller/ArticleController.java
@@ -1,20 +1,10 @@
 package com.rdbf.demo.apiauth.controller;
 
-import com.rdbf.demo.apiauth.controller.form.UserForm;
 import com.rdbf.demo.apiauth.domain.Article;
-import com.rdbf.demo.apiauth.domain.People;
-import com.rdbf.demo.apiauth.service.SampleService;
-import com.rdbf.demo.apiauth.support.SecurityConstants;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.*;
 import com.rdbf.demo.apiauth.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -38,10 +28,29 @@ public class ArticleController {
     }
 
     @GetMapping(value = "/article/{id}")
-    Article one(@PathVariable Long id) {
+    Article findByArticleId(@PathVariable Integer id) {
 
         return articleRepository.findByArticleId(id);
     }
+
+    @PostMapping(value = "/article")
+    public void createArticle( @RequestBody Article creteArticleData) {
+
+
+        articleRepository.createArticle(creteArticleData);
+
+    }
+
+    @PutMapping(value = "/article")
+    public void updateArticle( @RequestBody Article updateArticleData) {
+
+
+        articleRepository.updateArticle(updateArticleData);
+
+    }
+
+
+
 
 
 
