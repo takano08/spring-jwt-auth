e1adfb8eac4e918dad56043fe99dd09fa65fcbac
takano <ri1saxxminny83xx@gmail.com>
2020-08-23 21:31:31 +0900

    delete,update文追加、修正

--- src/main/java/com/rdbf/demo/apiauth/repository/ArticleRepository.java
+++ src/main/java/com/rdbf/demo/apiauth/repository/ArticleRepository.java
@@ -1,25 +1,25 @@
 package com.rdbf.demo.apiauth.repository;
 
 import com.rdbf.demo.apiauth.domain.Article;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
 public interface ArticleRepository {
     //articlesの指定したカラムをselectする
-    @Select("SELECT article_id,title,created_date,tag,body,owner,  FROM articles WHERE article_id = #{articleId}")
+    @Select("SELECT article_id,title,created_date,tag,body,owner FROM articles WHERE article_id = #{articleId}")
     Article findByArticleId(Integer articleId);
 
-    @Select("SELECT article_id,title,created_date,tag,body,owner,  FROM articles")
+    @Select("SELECT article_id,title,created_date,tag,body,owner FROM articles")
     List<Article> findAll();
 
     @Insert("INSERT INTO articles(article_id,title,created_date,tag,body,owner) VALUES (#{articleId},#{title},#{createdDate},#{tag},#{body},#{owner})")
     void createArticle(Article createArticleData);
 
-    @Update("UPDATE articles SET title=#{title},created_date=#{createdDate},tag=#{tag},body=#{body},owner=#{owner}")
+    @Update("UPDATE articles SET title=#{title},created_date=#{createdDate},tag=#{tag},body=#{body},owner=#{owner} WHERE article_id = #{articleId}")
     void updateArticle(Article updateArticleData);
+
+    @Delete("DELETE FROM articles WHERE article_id = #{articleId}")
+    void deleteByArticleId(Integer articleId);
 }
